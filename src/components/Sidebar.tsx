010608e7b58d8d0d4c93ccc687763ed1860fbb4a
wharem85 <wharem85@hotmail.com>
2024-02-22 08:30:30 -0600

    se agrego el toml

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import { Box, CssBaseline, Divider, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar } from '@mui/material';
 import {Home, ViewList, Person} from '@mui/icons-material';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useHref } from 'react-router-dom';
 import logo from '../img/logo.jpg'
 
 interface props {
@@ -56,35 +56,28 @@ const Sidebar: React.FC<props> = () => {
         <Divider />
         <List>
           <ListItem disablePadding sx={{ color: '#757575' }}>
-            <a href='/'>
-
             <ListItemButton sx={{ borderRadius: '15px', margin: '15px',backgroundColor: location.pathname === '/' ? '#fff' : 'transparent', '&:hover': { backgroundColor: '#fff', '& .MuiListItemIcon-root, & .MuiListItemText-primary': { color: '#757575' } } }}>
               <ListItemIcon sx={{ color: location.pathname === '/' ? '#757575' : '#fff' }}>
                 <Home />
               </ListItemIcon>
               <ListItemText primary={"Inicio"} sx={{ color: location.pathname === '/' ? '#757575' : '#fff' }} />
             </ListItemButton>
-            </a>
           </ListItem>
 					<ListItem disablePadding sx={{ color: '#757575' }}>
-            <a href='/perfil'>
-            <ListItemButton sx={{ borderRadius: '15px', margin: '15px',backgroundColor: location.pathname === '/perfil' ? '#fff' : 'transparent', '&:hover': { backgroundColor: '#fff', '& .MuiListItemIcon-root, & .MuiListItemText-primary': { color: '#757575' } } }} >
+            <ListItemButton href='/perfil' sx={{ borderRadius: '15px', margin: '15px',backgroundColor: location.pathname === '/perfil' ? '#fff' : 'transparent', '&:hover': { backgroundColor: '#fff', '& .MuiListItemIcon-root, & .MuiListItemText-primary': { color: '#757575' } } }} >
               <ListItemIcon sx={{ color: location.pathname === '/perfil' ? '#757575' : '#fff' }}>
                 <Person />
               </ListItemIcon>
               <ListItemText primary={"Perfil"} sx={{ color: location.pathname === '/perfil' ? '#757575' : '#fff' }} />
             </ListItemButton>
-            </a>
           </ListItem>
 					<ListItem disablePadding sx={{ color: '#757575' }}>
-            <a href='/vista' >
-            <ListItemButton sx={{ borderRadius: '15px', margin: '15px',backgroundColor: location.pathname === '/vista' ? '#fff' : 'transparent', '&:hover': { backgroundColor: '#fff', '& .MuiListItemIcon-root, & .MuiListItemText-primary': { color: '#757575' } } }}>
+            <ListItemButton href='/vista' sx={{ borderRadius: '15px', margin: '15px',backgroundColor: location.pathname === '/vista' ? '#fff' : 'transparent', '&:hover': { backgroundColor: '#fff', '& .MuiListItemIcon-root, & .MuiListItemText-primary': { color: '#757575' } } }}>
               <ListItemIcon sx={{ color: location.pathname === '/vista' ? '#757575' : '#fff' }}>
                 <ViewList />
               </ListItemIcon>
               <ListItemText primary={"Vista"} sx={{ color: location.pathname === '/vista' ? '#757575' : '#fff' }}/>
             </ListItemButton>
-            </a>
           </ListItem>
         </List>
       </Drawer>
